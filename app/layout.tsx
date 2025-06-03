39382a24f8ab4f455016132de4d71ca14fb4e17d
Henny Sánchez <kritikus.la@gmail.com>
2025-06-04 01:06:07 +0200

    Add speed up to the app by using a CDN for the fon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,35 +5,35 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { AuthProvider } from '@/context/auth'
 import { Ubuntu, Lato, Poppins, Outfit } from 'next/font/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
+//const ubuntu = Ubuntu({
+  //subsets: ['latin'],
+  //weight: ['300', '400', '500', '700'],
+  //variable: '--font-ubuntu',
+//})
 
-const ubuntu = Ubuntu({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '700'],
-  variable: '--font-ubuntu',
-})
+//const lato = Lato({
+  //subsets: ['latin'],
+  //weight: ['300', '400', '700'],
+  //variable: '--font-lato',
+  //display: 'swap',
+  //style: 'normal',
+  //fallback: ['system-ui', 'sans-serif'],
+  //preload: true,
+  //adjustFontFallback: true,
+  //})
 
-const lato = Lato({
-  subsets: ['latin'],
-  weight: ['300', '400', '700'],
-  variable: '--font-lato',
-  display: 'swap',
-  style: 'normal',
-  fallback: ['system-ui', 'sans-serif'],
-  preload: true,
-  adjustFontFallback: true,
-  })
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600'],
-  variable: '--font-poppins',
-  display: 'swap',
-  style: 'normal',
-  fallback: ['system-ui', 'sans-serif'],
-  preload: true,
-  adjustFontFallback: true,
-})
+//const poppins = Poppins({
+  //subsets: ['latin'],
+  //weight: ['300', '400', '500', '600'],
+  //variable: '--font-poppins',
+  //display: 'swap',
+  //style: 'normal',
+  //fallback: ['system-ui', 'sans-serif'],
+  //preload: true,
+  //adjustFontFallback: true,
+//})
 
 const outfit = Outfit({ subsets: ['latin'],weight: ['200', '300', '400', '500', '600', '700'] ,  variable: '--font-outfit', fallback: ['system-ui', 'sans-serif'], display: 'swap', style: 'normal', preload: true, adjustFontFallback: true, })
 const inter = Inter({ subsets: ['latin'],weight: ['300', '400', '500'], variable: '--font-inter', fallback: ['system-ui', 'sans-serif'], display: 'swap', style: 'normal', preload: true, adjustFontFallback: true, })
@@ -57,7 +57,7 @@ export default function RootLayout({
         <AuthProvider>
           {children}
         </AuthProvider>
-        
+        <SpeedInsights />
       </body>
     </html>
   )
